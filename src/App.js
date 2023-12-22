898aa600b6cc31be6b0798ebdd6bc64091975a4b
khalid <khalidathar96@gmail.com>
2023-12-22 20:23:20 +0530

    To-do Application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+
+// import './App.css';
+import { useState } from "react";
+
 
 function App() {
+
+  const [todo,setTodo] = useState("");
+  const [todos,setTodos] = useState([]);
+
+  const addTodo = ()=>{
+    if(todo !== ""){
+    setTodos([...todos,todo]);
+    setTodo("");
+  }}
+
+  const deleteTodo = (text)=>{
+    const newTodos = todos.filter((todo)=>{
+      return todo !== text;
+    });
+    setTodos(newTodos);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>TO DO APP</h1>
+        <div>
+          <input type="text" name="todo" value={todo} placeholder="create your todo"
+          onChange={(e)=>{setTodo(e.target.value)}}/>
+          <button onClick={addTodo}>ADD</button>
+        </div>
+
+       {todos?.length>0 ?(<ul>
+          {todos.map((todo,index)=>(
+            <div>
+              <li key={index}>{todo} <button onClick={()=>{deleteTodo(todo)}}>DELETE</button></li>
+            </div>
+          ))}
+        </ul>):(
+          <div>
+            <p>No task found</p>
+          </div>
+        )} 
     </div>
   );
 }
